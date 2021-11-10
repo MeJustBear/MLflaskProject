a245ca5c7df6c9bfcd0070cc0892906b05f82ad4
pavel <PavelFomBe;@yandex.ru>
2021-11-10 12:29:23 +0300

    Update docker-compose.yml

--- app/app.py
+++ app/app.py
@@ -1,26 +1,24 @@
 from flask import Flask, request, jsonify
 import config
-# from app.models.predict import model_predict as mp
-
 
 application = Flask(__name__)
 application.config.from_object(config.DevelopementConfig)
 
+from .models.predict import model_predict as mp
+
 
 @application.route('/predictByUrl', methods=['GET', 'POST'])
 def classify_url():  # put application's code here
-    # data = request.form
-    # values = mp.predict_by_url(data['analyseURL'])
-    # return jsonify(values)
-    return jsonify(config.forJSONexample.data)
-
+    data = request.form
+    values = mp.predict_by_url(data['analyseURL'])
+    return jsonify(values)
 
 @application.route('/predictByText', methods=['POST'])
 def classify_text():  # put application's code here
-    # data = request.form
-    # values = mp.predict_by_url(data['analyseURL'])
-    # return jsonify(values)
-    return jsonify(config.forJSONexample.data)
+    data = request.form
+    values = mp.predict_text(data['analyseURL'])
+    return jsonify(values)
+
 
 if __name__ == '__main__':
     application.run(host=config.ApplicationConfig.host, port=config.ApplicationConfig.port)
