439f7e332bea968ef2bd11ccc90ff2a039d2d822
MeJustBear <47248368+MeJustBear@users.noreply.github.com>
2021-11-11 02:52:36 +0300

    Update run.py
    Improve ecxeption

--- app/run.py
+++ app/run.py
@@ -10,14 +10,29 @@ from models.predict import model_predict as mp
 @application.route('/predictByUrl', methods=['GET', 'POST'])
 def classify_url():  # put application's code here
     data = request.form
-    values = mp.predict_by_url(data['analyseURL'])
-    return jsonify(values)
+    if data:
+        data = data['analyseURL']
+        if data:
+            values = mp.predict_by_url()
+            return jsonify(values)
+        if data:
+            return jsonify({})    
+    else:
+        return jsonify({})
+    
 
 @application.route('/predictByText', methods=['POST'])
 def classify_text():  # put application's code here
     data = request.form
-    values = mp.predict_text(data['analyseTEXT'])
-    return jsonify(values)
+    if data:
+        data = data['analyseTEXT']
+        if data:
+            values = mp.predict_text()
+            return jsonify(values)
+        if data:
+            return jsonify({})    
+    else:
+        return jsonify({})
 
 
 if __name__ == '__main__':
